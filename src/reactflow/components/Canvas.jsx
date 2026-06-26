bcb5bf0458b7b331ef47bec091f4ea5be0e8b319
abkr020 <abkr020@gmail.com>
2026-06-27 00:07:29 +0530

    -- undo this and leart

--- src/reactflow/components/Canvas.jsx
+++ src/reactflow/components/Canvas.jsx
@@ -4,6 +4,25 @@ import "./Canvas.css";
 export default function Canvas({ children }) {
   const [zoom, setZoom] = useState(1);
 
+  const [cursor, setCursor] = useState({
+    x: 0,
+    y: 0,
+  });
+
+  const rulerMarks = Array.from({ length: 80 }, (_, i) => i * 50);
+
+  function handleMove(e) {
+    const rect = e.currentTarget.getBoundingClientRect();
+console.log("rect",rect);
+console.log("clientX",e.clientX);
+console.log("clientY",e.clientY);
+
+    setCursor({
+      x: e.clientX - rect.left,
+      y: e.clientY - rect.top,
+    });
+  }
+
   return (
     <div className="canvas-wrapper">
       <div className="zoom-controls">
@@ -14,12 +33,67 @@ export default function Canvas({ children }) {
         </button>
       </div>
 
-      <div className="ruler-top"></div>
+      {/* TOP RULER */}
+      <div className="ruler-top">
+        {rulerMarks.map((n) => (
+          <div
+            key={n}
+            className="ruler-number-top"
+            style={{
+              left: `${n}px`,
+            }}
+          >
+            {n}
+          </div>
+        ))}
+
+        <div
+          className="cursor-v"
+          style={{
+            left: cursor.x,
+          }}
+        />
+      </div>
 
       <div className="canvas-body">
-        <div className="ruler-left"></div>
+        {/* LEFT RULER */}
 
-        <div className="canvas-viewport">
+        <div className="ruler-left">
+          {rulerMarks.map((n) => (
+            <div
+              key={n}
+              className="ruler-number-left"
+              style={{
+                top: `${n}px`,
+              }}
+            >
+              {n}
+            </div>
+          ))}
+
+          <div
+            className="cursor-h"
+            style={{
+              top: cursor.y,
+            }}
+          />
+        </div>
+
+        <div className="canvas-viewport" onMouseMove={handleMove}>
+          <div
+            className="cursor-v canvas-line"
+            style={{
+              left: cursor.x + 30,
+            }}
+          />
+
+          <div
+            className="cursor-h canvas-line"
+            style={{
+              top: cursor.y +30,
+            }}
+          />
+
           <div
             className="canvas-content"
             style={{
