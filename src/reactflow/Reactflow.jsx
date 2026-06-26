11c4e6e290ef675f6a6868111542ca60fe12814a
abkr020 <abkr020@gmail.com>
2026-06-26 22:07:44 +0530

    --next make the larege space area

--- src/reactflow/Reactflow.jsx
+++ src/reactflow/Reactflow.jsx
@@ -1,6 +1,36 @@
-import Flow from "./Flow";
+import { useState } from "react";
 import "./Flow.css";
+import "./Popup.css";
 
 export default function Reactflow() {
-  return <Flow />;
+  const [showPopup, setShowPopup] = useState(true);
+
+  return (
+    <div>
+      <button onClick={() => setShowPopup(true)}>Open Popup</button>
+
+      {showPopup && (
+        <div onClick={(e) => setShowPopup(false)} className="popup-overlay">
+          <div onClick={(e) => e.stopPropagation()} className="popup">
+            <div className="topbar">
+              <div>Top Bar</div>
+              <button
+                className="close-btn"
+                style={{ marginLeft: "auto" }}
+                onClick={() => setShowPopup(false)}
+              >
+                Close
+              </button>
+            </div>
+
+            <div className="content">
+              <div className="sidebar">Sidebar</div>
+
+              <div className="main">Main Container</div>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
